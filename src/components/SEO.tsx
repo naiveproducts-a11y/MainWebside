6c2f73558633c2da305eafad43863ad9a577b770
Napus Samuanpho <naiveproducts@gmail.com>
2026-06-20 20:19:34 +0700

    feat: display brand prefix in tab title instead of suffix

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -22,7 +22,7 @@ export default function SEO({
 
   useEffect(() => {
     // 1. Title
-    const formattedTitle = title.includes('naive innova') || title.includes('Naive Innova') ? title : `${title} | naive innova`;
+    const formattedTitle = title.includes('naive innova') || title.includes('Naive Innova') ? title : `naive innova | ${title}`;
     document.title = formattedTitle;
 
     // 2. Meta description
